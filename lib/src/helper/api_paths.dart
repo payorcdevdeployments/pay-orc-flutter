9d58d3ef4c007bcb221eb4a4718a85cd1b30b9f7
esaiamuthan <isaiamuthan.r@oclocksoftware.com>
2025-02-11 08:29:30 +0530

    Updated end point for fetch transaction

--- lib/src/helper/api_paths.dart
+++ lib/src/helper/api_paths.dart
@@ -2,5 +2,5 @@ class ApiPaths {
   static const String URL_CHECK_KEYS = "/check/keys-secret";
   static const String URL_CREATE_PAYMENT = "/sdk/orders/create";
   static const String URL_PAYMENT_TRANSACTION =
-      "/open/orders/transaction-details";
+      "/sdk/orders/transaction-details";
 }
